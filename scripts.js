a2ff0fd2dab12a1a3024a4a027d8e1d69aedf345
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-05-21 16:42:44 +0200

    Iteracion#1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-console.log('probando');
+const btnToClick = document.querySelector('#btnToClick');
+const inputClick = document.querySelector('.focus');
+const inputOnInput = document.querySelector('.value');
+
+function printInConsole(event) { console.log(event); };
+
+function getInputValue(value) {
+    value = this.value;
+    console.log(value);
+};
+
+function getValueOnInput(value) {
+    value = this.value;
+    console.log(value);
+}
+
+
+btnToClick.addEventListener('click', printInConsole);
+// Tal y como está planteado el ejercicio, en el caso del focus ya que no hay un value predefinido en el input, es necesario poner el foco en el input, escribir algo, poner el foco en otro lado del documento y volver a poner el foco en el mismo input.
+inputClick.addEventListener('focus', getInputValue);
+inputOnInput.addEventListener('input', getValueOnInput);
